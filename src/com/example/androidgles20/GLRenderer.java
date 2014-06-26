c1b75ed374213f66b10ffa59ea7f0d3c042a011b
ryngash <nagashi.nishi@gmail.com>
2014-06-27 00:26:16 +0900

    Sample

--- src/com/example/androidgles20/GLRenderer.java
+++
@@ -1,5 +0,0 @@
-package com.example.androidgles20;
-
-public class GLRenderer {
-
-}
--- src/com/example/androidgles20/MainActivity.java
+++ src/com/example/androidgles20/MainActivity.java
@@ -1,15 +1,50 @@
 package com.example.androidgles20;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+    private GLSurfaceView view = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        view = (GLSurfaceView) findViewById(R.id.surfaceView);
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+
+        // 端末がOpenGL ES 2.0をサポートしているかチェック
+        if (configurationInfo.reqGlEsVersion >= 0x20000) {
+            view.setEGLContextClientVersion(2); // OpenGLバージョンを設定
+            view.setRenderer(new MyRenderer()); // レンダラを設定
+
+        } else {
+            finish();
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (view != null) {
+            view.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (view != null) {
+            view.onPause();
+        }
     }
 }
